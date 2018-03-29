6fb5231a97b5d0502b52b65c004a9ed0cea94f6d
unknown <vsuper>
2018-03-29 16:03:00 +0800

    优化

--- request.go
+++ request.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/axgle/mahonia"
-	// "io"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -16,17 +16,15 @@ import (
 )
 
 type RequestOption struct {
-	Method       string
-	Url          string
-	Header       http.Header
-	ConnTimeout  time.Duration     // 链接超时时间
-	Timeout      time.Duration     // 传输超时时间
-	Retrytimes   int               // 重试次数
-	DelayTime    time.Duration     // 重试间隔时间
-	ParamsData   map[string]string // GET参数
-	FormData     url.Values        // post data
-	JsonFormData *bytes.Reader     // json post data
-	JsonContent  bool              // Content-Type是否为json
+	Method      string
+	Url         string
+	HeaderData  http.Header
+	ConnTimeout time.Duration     // 链接超时时间
+	Timeout     time.Duration     // 传输超时时间
+	Retrytimes  int               // 重试次数
+	DelayTime   time.Duration     // 重试间隔时间
+	ParamsData  map[string]string // GET参数
+	FormData    io.Reader         // post data
 }
 
 type Response struct {
@@ -43,6 +41,7 @@ func Request(url string, method string) *RequestOption {
 		Timeout:     15,
 		Retrytimes:  3,
 		DelayTime:   3,
+		HeaderData:  make(http.Header),
 	}
 }
 
@@ -62,19 +61,29 @@ func (option *RequestOption) Params(params map[string]string) *RequestOption {
 
 // post form data
 func (option *RequestOption) Form(data url.Values) *RequestOption {
-	option.FormData = data
+	option.HeaderData.Set("Content-Type", "application/x-www-form-urlencoded")
+	reader := strings.NewReader(data.Encode())
+	option.FormData = reader
 	return option
 }
 
 // json post form data
 func (option *RequestOption) JsonForm(data *map[string]interface{}) *RequestOption {
-	option.JsonContent = true
+	option.HeaderData.Set("Content-Type", "application/json;charset=UTF-8")
 	bytesData, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err.Error())
 		return option
 	}
-	option.JsonFormData = bytes.NewReader(bytesData)
+	option.FormData = bytes.NewReader(bytesData)
+	return option
+}
+
+// header option
+func (option *RequestOption) Header(h map[string]string) *RequestOption {
+	for k, v := range h {
+		option.HeaderData.Set(k, v)
+	}
 	return option
 }
 
@@ -96,29 +105,19 @@ func (option *RequestOption) Open() *Response {
 	}
 
 	// request
-	var req *http.Request
-	var reqerr error
-	if option.JsonContent {
-		req, reqerr = http.NewRequest(option.Method, option.Url, option.JsonFormData) // Content-Type 为json时
-	} else {
-		req, reqerr = http.NewRequest(option.Method, option.Url, option.FormData)
-	}
+	req, reqerr := http.NewRequest(option.Method, option.Url, option.FormData)
 	if reqerr != nil {
 		result.Error = reqerr
 		return result
 	}
 
+	// header 设置
+	req.Header = option.HeaderData
+
 	// get 参数
 	q := req.URL.Query()
 	for k, v := range option.ParamsData {
 		q.Add(k, v)
-	}
-
-	// 默认headers
-	if option.JsonContent {
-		req.Header.Set("Content-Type", "application/json;charset=UTF-8")
-	} else {
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	}
 
 	// 请求失败重新请求
@@ -171,10 +170,12 @@ func gethost(url string) string {
 	return strings.Split(a1, "/")[0]
 }
 
+// 获取原生网页
 func (response *Response) Native() (*http.Response, error) {
 	return response.Response, response.Error
 }
 
+// 转换为json
 func (response *Response) Json(t interface{}) error {
 	resp := response.Response
 	if response.Error != nil {
@@ -189,21 +190,7 @@ func (response *Response) Json(t interface{}) error {
 	return err
 }
 
-// jsonp to json
-// func (response *Response) Jsonp() (*simplejson.Json, error) {
-// 	defer response.Response.Body.Close()
-// 	body, _ := ioutil.ReadAll(response.Response.Body)
-
-// 	// match json
-// 	reg := regexp.MustCompile(`^[^\[{]*([\[{][\s\S]*?[\]}])[^\]}]*$`)
-// 	match := reg.FindSubmatch(body)
-// 	if len(match) < 2 {
-// 		return nil, errors.New("jsonp to json error")
-// 	}
-
-// 	return simplejson.NewJson(match[1])
-// }
-
+// 转换为goquery
 func (response *Response) Html() (*goquery.Document, error) {
 	if response.Error != nil {
 		return nil, response.Error
